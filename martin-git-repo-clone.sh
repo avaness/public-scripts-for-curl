e96f06b100bca498e8a781ac0d5f11396287c027
avaness <ivan.lazarev@gmail.com>
2020-07-06 00:12:54 +0300

    Update martin-git-repo-clone.sh

--- martin-git-repo-clone.sh
+++ martin-git-repo-clone.sh
@@ -9,11 +9,11 @@
 # updating scripts - clone repo or pulling updates
 cd ${HOME}
 pwd
-export GIT_DIR=app-eth2
-GIT_REPO=https://github.com/ngin-io/$GIT_DIR.git
-GIT_BRANCH=eth-try
-if cd $GIT_DIR; then git pull; else git clone $GIT_REPO && cd $GIT_DIR && git config credential.helper store; fi
-git checkout $GIT_BRANCH
+export REPo_DIR=app-eth2
+REPO=https://github.com/ngin-io/$GIT_DIR.git
+BRANCH=eth-try
+if cd $REPO_DIR; then git pull; else git clone $REPO && cd $REPO_DIR && git config credential.helper store; fi
+git checkout $BRANCH
 
 # running and entering docker sandbox
 # TODO REVIEW usage below
