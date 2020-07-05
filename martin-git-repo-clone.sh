b9a6c31424079d30f85ad70e811516881a9f75fa
avaness <ivan.lazarev@gmail.com>
2020-07-06 00:15:57 +0300

    Update martin-git-repo-clone.sh

--- martin-git-repo-clone.sh
+++ martin-git-repo-clone.sh
@@ -10,7 +10,7 @@
 cd ${HOME}
 pwd
 export REPO_DIR=app-eth2
-REPO=https://github.com/ngin-io/$GIT_DIR.git
+REPO=https://github.com/ngin-io/$REPO_DIR.git
 BRANCH=eth-try
 if cd $REPO_DIR; then git pull; else git clone $REPO && cd $REPO_DIR && git config credential.helper store; fi
 git checkout $BRANCH
